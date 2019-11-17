18893341c153f9f42a44fa20e67f20a4bd653632
Dominykas Kutka <dominykaskulka@yahoo.com>
2019-11-17 18:23:31 +0100

    Fixed falling star movement

--- javascript.js
+++ javascript.js
@@ -1,10 +1,12 @@
-let timelineContainer = document.querySelector('#timeline-container');
-let timelineSVGLink = './assets/NEWtimeline.svg';
+let timelineContainer = document.querySelector("#timeline-container");
+let timelineSVGLink = "./assets/NEWtimeline.svg";
 
-fetch(timelineSVGLink).then((e) => e.text()).then((svgData) => appendSvgData(svgData));
+fetch(timelineSVGLink)
+  .then(e => e.text())
+  .then(svgData => appendSvgData(svgData));
 
 function appendSvgData(svgData) {
-	timelineContainer.innerHTML += svgData;
+  timelineContainer.innerHTML += svgData;
 }
 
 // Useful ids for timeline:
@@ -14,65 +16,71 @@ function appendSvgData(svgData) {
 // #timeline-svg for the timeline svg
 // .fallingStar for moving star circle
 
-let dataPointsLink = './data-points.json';
+let dataPointsLink = "./data-points.json";
 let yearsArray = [];
 let futureYearsArray = [];
 let totalYearsArray = [];
-let yDataPointsArray = [ 450 ];
-let yFutureDataPointsArray = [ 2450 ];
+let yDataPointsArray = [450];
+let yFutureDataPointsArray = [2450];
 let yTotalDataPointsArray = [];
 let globalDataEvents = [];
 
-fetch(dataPointsLink).then((e) => e.json()).then((dataEvents) => showDataEvents(dataEvents));
+fetch(dataPointsLink)
+  .then(e => e.json())
+  .then(dataEvents => showDataEvents(dataEvents));
 
 function showDataEvents(dataEvents) {
-	globalDataEvents = dataEvents;
-	let keys = Object.keys(dataEvents[0]);
-	let lengthObject = keys.length;
-	for (let i = 0; i < lengthObject; i++) {
-		if (dataEvents[0][keys[i]]['time'] == 'Past') {
-			yearsArray.push(dataEvents[0][keys[i]]['year-of-discovery']);
-		} else {
-			futureYearsArray.push(dataEvents[0][keys[i]]['year-of-discovery']);
-		}
-	}
+  globalDataEvents = dataEvents;
+  let keys = Object.keys(dataEvents[0]);
+  let lengthObject = keys.length;
+  for (let i = 0; i < lengthObject; i++) {
+    if (dataEvents[0][keys[i]]["time"] == "Past") {
+      yearsArray.push(dataEvents[0][keys[i]]["year-of-discovery"]);
+    } else {
+      futureYearsArray.push(dataEvents[0][keys[i]]["year-of-discovery"]);
+    }
+  }
 
-	populateTimeline();
+  populateTimeline();
 }
 
 // 1 year = 40;
 // y = 450 data-point start
 
 function populateTimeline() {
-	let dataPoint = document.querySelector('#data-point');
-	let parentContainer = document.querySelector('#parent-container');
-	let lineLength;
-	let startingYear = yearsArray[0];
-	parseInt(startingYear);
+  let dataPoint = document.querySelector("#data-point");
+  let parentContainer = document.querySelector("#parent-container");
+  let lineLength;
+  let startingYear = yearsArray[0];
+  parseInt(startingYear);
 
-	// Create data point circles and append them by year
+  // Create data point circles and append them by year
 
-	for (let i = 1; i < yearsArray.length; i++) {
-		let currentYear = yearsArray[i];
-		parseInt(currentYear);
+  for (let i = 1; i < yearsArray.length; i++) {
+    let currentYear = yearsArray[i];
+    parseInt(currentYear);
 
-		let yDistance = (currentYear - startingYear) * 40 + 498.5;
+    let yDistance = (currentYear - startingYear) * 40 + 498.5;
 
-		yDataPointsArray.push(yDistance);
+    yDataPointsArray.push(yDistance);
 
-		let cloneDP = dataPoint.cloneNode(true);
-		cloneDP.querySelector('.outer-circle').setAttribute('transform', `matrix(1,0,0,1,918.5,${yDistance})`);
-		cloneDP.querySelector('.inner-circle').setAttribute('transform', `matrix(1,0,0,1,918.5,${yDistance})`);
-		cloneDP.setAttribute('height', yDistance);
-		parentContainer.appendChild(cloneDP);
+    let cloneDP = dataPoint.cloneNode(true);
+    cloneDP
+      .querySelector(".outer-circle")
+      .setAttribute("transform", `matrix(1,0,0,1,918.5,${yDistance})`);
+    cloneDP
+      .querySelector(".inner-circle")
+      .setAttribute("transform", `matrix(1,0,0,1,918.5,${yDistance})`);
+    cloneDP.setAttribute("height", yDistance);
+    parentContainer.appendChild(cloneDP);
 
-		// Calculates total length of SVG
+    // Calculates total length of SVG
 
-		lineLength = (currentYear - startingYear) * 40 + 498.5;
-	}
+    lineLength = (currentYear - startingYear) * 40 + 498.5;
+  }
 
-	console.log(lineLength);
-	generateTimeline(lineLength);
+  console.log(lineLength);
+  generateTimeline(lineLength);
 }
 
 // q: to run an npm package like lodash, underscore you are required
@@ -84,85 +92,88 @@ function populateTimeline() {
 // Using throttle from underscore
 
 function moveFallingStar() {
+  window.addEventListener("scroll", _.throttle(changePosition, 100));
+  let body = document.querySelector("body");
 
-window.addEventListener("scroll", _.throttle(changePosition, 100));
-let body = document.querySelector("body");
-let fallingStar = document.querySelector(".fallingStar");	
+  function changePosition() {
+    // https://stackoverflow.com/questions/8584902/get-closest-number-out-of-array
 
-function changePosition() {
+    let goal = body.scrollTop;
+    let closest = yTotalDataPointsArray.reduce(function(prev, curr) {
+      return Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev;
+    });
 
-	// https://stackoverflow.com/questions/8584902/get-closest-number-out-of-array
-
-	let goal = body.scrollTop;
-	let closest = yTotalDataPointsArray.reduce(function(prev, curr) {
-		return (Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev);
-	});
-
-	setStarPosition(closest, fallingStar);
-}
+    setStarPosition(closest);
+  }
 }
 
-function setStarPosition(closest, fallingStar) {
-	fallingStar.setAttribute("transform", "matrix(1,0,0,1,918.5," + ( closest + 130 ) + ")");
-	console.log(fallingStar);
-	// ISSUE: Does not set itself?
+function setStarPosition(closest) {
+  document
+    .querySelector(".fallingStar")
+    .setAttribute("transform", "matrix(1,0,0,1,918.5," + (closest + 130) + ")");
+
+  //console.log(document.querySelector(".fallingStar"));
 }
 
 function createYearsTimeline() {
-	let yearsContainer = document.querySelector('#years-container');
-	totalYearsArray = yearsArray.concat(futureYearsArray);
+  let yearsContainer = document.querySelector("#years-container");
+  totalYearsArray = yearsArray.concat(futureYearsArray);
 
-	for (let i = 0; i < totalYearsArray.length; i++) {
-		let p = document.createElement('p');
-		p.textContent = totalYearsArray[i];
-		p.setAttribute('class', 'yearParagraph');
-		yearsContainer.appendChild(p);
-	}
-	positionYearsTimeline();
+  for (let i = 0; i < totalYearsArray.length; i++) {
+    let p = document.createElement("p");
+    p.textContent = totalYearsArray[i];
+    p.setAttribute("class", "yearParagraph");
+    yearsContainer.appendChild(p);
+  }
+  positionYearsTimeline();
 }
 
 function positionYearsTimeline() {
-	let yearsParagraphsArray = document.querySelectorAll('.yearParagraph');
-	let lastVal = yDataPointsArray[yDataPointsArray.length - 1];
+  let yearsParagraphsArray = document.querySelectorAll(".yearParagraph");
+  let lastVal = yDataPointsArray[yDataPointsArray.length - 1];
 
-	for (let i = 0; i < yFutureDataPointsArray.length; i++) {
-		yFutureDataPointsArray[i] += lastVal;
-		yFutureDataPointsArray[i] -= 1970;
-	}
+  for (let i = 0; i < yFutureDataPointsArray.length; i++) {
+    yFutureDataPointsArray[i] += lastVal;
+    yFutureDataPointsArray[i] -= 1970;
+  }
 
-	console.log(yearsParagraphsArray);
+  console.log(yearsParagraphsArray);
 
-	yTotalDataPointsArray = yDataPointsArray.concat(yFutureDataPointsArray);
+  yTotalDataPointsArray = yDataPointsArray.concat(yFutureDataPointsArray);
 
-	console.log(yTotalDataPointsArray);
+  console.log(yTotalDataPointsArray);
 
-	for (let i = 0; i < yTotalDataPointsArray.length; i++) {
-		yearsParagraphsArray[i].style.marginTop = yTotalDataPointsArray[i] + 'px';
-	}
+  for (let i = 0; i < yTotalDataPointsArray.length; i++) {
+    yearsParagraphsArray[i].style.marginTop = yTotalDataPointsArray[i] + "px";
+  }
 
-	moveFallingStar();
+  moveFallingStar();
 }
 
 function generateTimeline(lineLength) {
-	document.querySelector('#timeline-svg').setAttribute('height', lineLength);
-	document.querySelector('#timeline-svg').setAttribute('viewBox', `0 0 84 ${lineLength}`);
-	document.querySelector('.timeline-line').setAttribute('y2', lineLength);
-	document.querySelector('.filter-line').setAttribute('height', lineLength);
+  document.querySelector("#timeline-svg").setAttribute("height", lineLength);
+  document
+    .querySelector("#timeline-svg")
+    .setAttribute("viewBox", `0 0 84 ${lineLength}`);
+  document.querySelector(".timeline-line").setAttribute("y2", lineLength);
+  document.querySelector(".filter-line").setAttribute("height", lineLength);
 
-	fetchFutureTimeline();
+  fetchFutureTimeline();
 }
 
 // add second half of timeline
 
 function fetchFutureTimeline() {
-	let futureTimelineSVGLink = './assets/timeline-after2019.svg';
-	fetch(futureTimelineSVGLink).then((e) => e.text()).then((svgData) => addFutureTimeline(svgData));
+  let futureTimelineSVGLink = "./assets/timeline-after2019.svg";
+  fetch(futureTimelineSVGLink)
+    .then(e => e.text())
+    .then(svgData => addFutureTimeline(svgData));
 }
 
 function addFutureTimeline(svgData) {
-	timelineContainer.innerHTML += svgData;
-	populateFutureTimeline();
-	fetchEndTimeline();
+  timelineContainer.innerHTML += svgData;
+  populateFutureTimeline();
+  fetchEndTimeline();
 }
 
 // #Repeat_Grid_1 for the first set of dashed lines
@@ -173,100 +184,109 @@ function addFutureTimeline(svgData) {
 // y = 2450 data-point start
 
 function populateFutureTimeline() {
-	// Same as with the past timeline
+  // Same as with the past timeline
 
-	let dataPoint = document.querySelector('#future-data-point');
-	let parentContainer = document.querySelector('#future-parent-container');
-	console.log(futureYearsArray);
+  let dataPoint = document.querySelector("#future-data-point");
+  let parentContainer = document.querySelector("#future-parent-container");
+  console.log(futureYearsArray);
 
-	let startingYear = futureYearsArray[0];
-	parseInt(startingYear);
+  let startingYear = futureYearsArray[0];
+  parseInt(startingYear);
 
-	for (let i = 1; i < futureYearsArray.length; i++) {
-		let currentYear = futureYearsArray[i];
-		parseInt(currentYear);
+  for (let i = 1; i < futureYearsArray.length; i++) {
+    let currentYear = futureYearsArray[i];
+    parseInt(currentYear);
 
-		let yDistance = (currentYear - startingYear) * 240 + 2450;
+    let yDistance = (currentYear - startingYear) * 240 + 2450;
 
-		yFutureDataPointsArray.push(yDistance);
+    yFutureDataPointsArray.push(yDistance);
 
-		let cloneDP = dataPoint.cloneNode(true);
-		cloneDP.querySelector('.outer-circle').setAttribute('transform', `matrix(1,0,0,1,918.5,${yDistance})`);
-		cloneDP.querySelector('.inner-circle').setAttribute('transform', `matrix(1,0,0,1,918.5,${yDistance})`);
-		parentContainer.appendChild(cloneDP);
-	}
+    let cloneDP = dataPoint.cloneNode(true);
+    cloneDP
+      .querySelector(".outer-circle")
+      .setAttribute("transform", `matrix(1,0,0,1,918.5,${yDistance})`);
+    cloneDP
+      .querySelector(".inner-circle")
+      .setAttribute("transform", `matrix(1,0,0,1,918.5,${yDistance})`);
+    parentContainer.appendChild(cloneDP);
+  }
 
-	createYearsTimeline();
+  createYearsTimeline();
 }
 
 function fetchEndTimeline() {
-	let endTimeline = './assets/timeline-end.svg';
-	fetch(endTimeline).then((e) => e.text()).then((svgData) => addTimelineEnd(svgData));
+  let endTimeline = "./assets/timeline-end.svg";
+  fetch(endTimeline)
+    .then(e => e.text())
+    .then(svgData => addTimelineEnd(svgData));
 }
 
 // margin-left added as an inline style to the end element
 
 function addTimelineEnd(svgData) {
-	timelineContainer.innerHTML += svgData;
-	addIconsTimeline();
+  timelineContainer.innerHTML += svgData;
+  addIconsTimeline();
 }
 
 function addIconsTimeline() {
-	let parentContainer = document.querySelector('#svg-icons');
+  let parentContainer = document.querySelector("#svg-icons");
 
-	// because it is an object you can go through it through object.keys
+  // because it is an object you can go through it through object.keys
 
-	let keys = Object.keys(globalDataEvents[0]);
-	let lengthObject = keys.length;
-	for (let i = 0; i < lengthObject; i++) {
-		let img = document.createElement('img');
-		img.src = `./assets/${globalDataEvents[0][keys[i]]['image-link']}.png`;
-		img.setAttribute('class', 'svg-icon');
-		parentContainer.appendChild(img);
-	}
-	positionIcons();
+  let keys = Object.keys(globalDataEvents[0]);
+  let lengthObject = keys.length;
+  for (let i = 0; i < lengthObject; i++) {
+    let img = document.createElement("img");
+    img.src = `./assets/${globalDataEvents[0][keys[i]]["image-link"]}.png`;
+    img.setAttribute("class", "svg-icon");
+    parentContainer.appendChild(img);
+  }
+  positionIcons();
 }
 
 function positionIcons() {
-	let icons = document.querySelectorAll('.svg-icon');
+  let icons = document.querySelectorAll(".svg-icon");
 
-	for (let i = 0; i < yTotalDataPointsArray.length; i++) {
-		icons[i].style.marginTop = yTotalDataPointsArray[i] + 'px';
-	}
+  for (let i = 0; i < yTotalDataPointsArray.length; i++) {
+    icons[i].style.marginTop = yTotalDataPointsArray[i] + "px";
+  }
 
-	addEventsDP();
+  addEventsDP();
 }
 
 function addEventsDP() {
-	let dataPoints = document.querySelectorAll('.general-data-point');
-	let i = 0;
-	dataPoints.forEach((el) => {
-		el.classList.add('dp-no' + i);
-		i++;
-	});
-	dataPoints.forEach((el) => el.addEventListener('click', showDetails));
+  let dataPoints = document.querySelectorAll(".general-data-point");
+  let i = 0;
+  dataPoints.forEach(el => {
+    el.classList.add("dp-no" + i);
+    i++;
+  });
+  dataPoints.forEach(el => el.addEventListener("click", showDetails));
 }
 
-let threeBranchLink = './assets/NEWconstelation.svg';
+let threeBranchLink = "./assets/NEWconstelation.svg";
 
 function showDetails() {
-	// search for any previous instances open
+  // search for any previous instances open
 
-	document.querySelector('#three-line-svg') != undefined
-		? document.querySelector('#three-line-svg').remove()
-		: console.log('ok');
+  document.querySelector("#three-line-svg") != undefined
+    ? document.querySelector("#three-line-svg").remove()
+    : console.log("ok");
 
-	// getBoundingClientRect gives you relative coords to the whole doc
-	// let coordsClickedObj = event.target.getBoundingClientRect();
+  // getBoundingClientRect gives you relative coords to the whole doc
+  // let coordsClickedObj = event.target.getBoundingClientRect();
 
-	let dpNo = event.target.parentElement.parentElement.parentElement.classList[1];
-	dpNo = dpNo.slice(5);
+  let dpNo =
+    event.target.parentElement.parentElement.parentElement.classList[1];
+  dpNo = dpNo.slice(5);
 
-	fetch(threeBranchLink).then((e) => e.text()).then((svgData) => {
-		let parentContainer = document.querySelector('#svg-info');
-		parentContainer.innerHTML += svgData;
-		positionSVGInfo(dpNo);
-	});
+  fetch(threeBranchLink)
+    .then(e => e.text())
+    .then(svgData => {
+      let parentContainer = document.querySelector("#svg-info");
+      parentContainer.innerHTML += svgData;
+      positionSVGInfo(dpNo);
+    });
 }
 
 // #line-three-svg id for the group itself
@@ -276,9 +296,9 @@ function showDetails() {
 // difference between line center and bottom-left infobox x = 388 px
 
 function positionSVGInfo(dpNo) {
-	let svgBranch = document.querySelector('#three-line-svg');
-	svgBranch.style.marginTop = yTotalDataPointsArray[dpNo] - 435 + 'px';
-	addInfoboxes(dpNo);
+  let svgBranch = document.querySelector("#three-line-svg");
+  svgBranch.style.marginTop = yTotalDataPointsArray[dpNo] - 435 + "px";
+  addInfoboxes(dpNo);
 }
 
 // ISSUE - with two very close data points you cannot click on the other one
@@ -286,12 +306,14 @@ function positionSVGInfo(dpNo) {
 // without pos absolute the whole design breaks.
 
 function addInfoboxes(dpNo) {
-	let infoboxes = document.querySelector('.infoboxes');
-	infoboxes.style.display = 'block';
-	let keys = Object.keys(globalDataEvents[0]);
-	infoboxes.querySelector('h2').textContent = globalDataEvents[0][keys[dpNo]]['events'];
-	infoboxes.querySelector('.country-involved').textContent = globalDataEvents[0][keys[dpNo]]['country-organization'];
-	infoboxes.querySelector('.memorable-contributors').textContent =
-		globalDataEvents[0][keys[dpNo]]['memorable-contributors'];
-	infoboxes.style.marginTop = yTotalDataPointsArray[dpNo] - 435 + 'px';
+  let infoboxes = document.querySelector(".infoboxes");
+  infoboxes.style.display = "block";
+  let keys = Object.keys(globalDataEvents[0]);
+  infoboxes.querySelector("h2").textContent =
+    globalDataEvents[0][keys[dpNo]]["events"];
+  infoboxes.querySelector(".country-involved").textContent =
+    globalDataEvents[0][keys[dpNo]]["country-organization"];
+  infoboxes.querySelector(".memorable-contributors").textContent =
+    globalDataEvents[0][keys[dpNo]]["memorable-contributors"];
+  infoboxes.style.marginTop = yTotalDataPointsArray[dpNo] - 435 + "px";
 }
